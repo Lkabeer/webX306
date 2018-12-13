ed76010475e4661068fa8f6a02b1a0665cfd7a88
Ahmed Samir <ahmedsamir@Ahmeds-MacBook-Pro.local>
2018-12-13 21:06:42 +0200

    version X1.3

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'loginX',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    
+  }
+
+}
